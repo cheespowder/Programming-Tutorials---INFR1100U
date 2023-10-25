41990dbc17940c8c138700959efdbcbf091ca5e2
cheespowder <36488998+cheespowder@users.noreply.github.com>
2023-10-25 00:49:44 -0400

    Initial commit

---
+++ Assets/Scipts/Managers/InputManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class InputManager
+{
+
+    public static GameControls _GameControls;
+    
+    public static void init(Player myPlayer)
+    {
+        _GameControls = new GameControls();
+        
+        _GameControls.Permanent.Enable();
+        
+        _GameControls.InGame.Movement.performed += jude =>
+        {
+            myPlayer.SetMovementDirection(jude.ReadValue<Vector3>());
+        };
+    }
+
+
+    public static void SetGameControls()
+    {
+        _GameControls.InGame.Enable();
+        _GameControls.UI.Disable();
+    }
+
+    public static void SetUIControls()
+    {
+        _GameControls.UI.Enable();
+        _GameControls.InGame.Disable();
+    }
+
+
+
+
+
+}
